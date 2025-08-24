fd6a367514084146478d093fc8c6c32d62c83711
beefstew31 <beefstewart3190@gmail.com>
2025-08-24 10:39:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Players from "./Players.jsx";
-import TeamPage from "./TeamPage.jsx";
-import Upload from "./Upload.jsx";
+import Players from "./Players.jsx";       // list page
+import PlayerPage from "./PlayerPage.jsx"; // individual page
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/season/2024" replace />} />
-        <Route path="/season/:season/player/:playerSlug" element={<Players />} />
-        <Route path="/season/:season/team/:teamName" element={<TeamPage />} />
-        <Route path="/admin/upload" element={<Upload />} />
+        <Route path="/" element={<Navigate to="/season/2025/players" replace />} />
+        <Route path="/season/:season/players" element={<Players />} />
+        <Route path="/season/:season/player/:playerSlug" element={<PlayerPage />} />
+        {/* ... your other routes ... */}
       </Routes>
     </BrowserRouter>
   );
